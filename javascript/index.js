59c23779719b49d68c7f52b6d774be59fa2b9315
bridgemami <mdbridgeman@gmail.com>
2022-10-23 15:30:31 -0700

    cleaned jup the reset

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,8 @@ let api = await fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes');
 let response = await api.json();
 		console.log(response[0]);
 		document.getElementById('button').addEventListener('click',
-		() => {document.getElementById('answer').innerHTML=`${response[0]}`
+		() => {document.getElementById('answer').style='block'
+			document.getElementById('answer').innerHTML=`${response[0]}`
 		eightBall()
 
 	}	
@@ -18,7 +19,7 @@ eightBall();
 
 function resets(){
 	document.getElementById('empty').addEventListener('click', ()=>{
-	document.getElementById('form').reset()
+	document.getElementById('clear').reset()
 	document.getElementById('answer').style.display='none';
 })
 }
